eb8129fdff92b64355417a0f30782595203946bc
Mohammad Ashoori <mohammadashoori.87@gmail.com>
2024-11-02 03:37:32 -0700

    [Get Values User Inputs]
    Get the three principal values (operator, prime number, second number)

--- Challenge-01/assets/js/main.js
+++ Challenge-01/assets/js/main.js
@@ -1,2 +1,4 @@
 // Invoking functions from a file "./funOperations.js"
 import * as funOperations from "./funOperations.js"
+// Get the three principal values (operator, prime number, second number)
+let operationInput, number1, number2
